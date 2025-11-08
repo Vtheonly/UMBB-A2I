026974196e21fa1d75e5b4ab4d9a6c6125002485
mersel fares <merselfaresw@gmail.com>
2025-11-08 06:06:01 +0100

    nchalah

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/Vtheonly",
+      "Linkedin": "https://www.linkedin.com/in/mersel-fares-baa8aa268/",
     },
   }),
 }
